dac9a183f12a8c16d3ccd0e66d76581ce0bca4e9
ubisoft97 <mursalimov.97@mail.ru>
2016-05-18 20:30:08 +0600

    Random

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <time.h>
+
+int getrand(int min, int max)
+{
+    return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
+double wtime()
+{
+    struct timeval t;
+    gettimeofday(&t, NULL);
+    return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
