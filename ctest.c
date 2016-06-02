939ad613fd54918b1cdb1ef0ded707a07e9f0449
ubisoft97 <mursalimov.97@mail.ru>
2016-06-02 23:50:16 +0600

    CTEST

---
+++ ctest.c
@@ -0,0 +1,19 @@
+#define CTEST_MAIN
+
+#include <stdio.h>
+#include <ctype.h>
+#include "ctest.h"
+#include "func.c"
+
+CTEST(solution_suite, easy) {
+
+    const int solution = check();
+
+    const int expected_solution = 20;
+
+    ASSERT_DBL_NEAR(expected_solution, solution);
+}
+
+int main (int argc, const char** argv) {
+	return ctest_main(argc, argv);
+}
